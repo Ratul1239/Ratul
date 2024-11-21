8f3b6349fbfd9475d0a111d41296a4ebdbf45847
Ratul1239 <ri373820@gmail.com>
2024-11-21 22:29:25 +0600

    Add files via upload

---
+++ Weak_04/Day_03/big_sum.cpp
@@ -0,0 +1,31 @@
+class Solution{
+  public:
+    int longestKSubstr(string s, int k) {
+        int n=s.size(),l=0,r=0,ans=-1;
+        map<char,int>mp;
+        while(r<n)
+        {
+            mp[s[r]]++;
+            if(mp.size()==k)
+            {
+                ans=max(ans,r-l+1);
+            }
+            else 
+            {
+                while(mp.size()>k&&1<=r)
+                {
+                    mp[s[l]]--;
+                    if(mp[s[l]]==0)
+                    {
+                        mp.erase(s[l]);
+                    }
+                    l++;
+                }
+                
+            }
+            r++;
+        }
+        return ans;
+    // your code here
+    }
+};
