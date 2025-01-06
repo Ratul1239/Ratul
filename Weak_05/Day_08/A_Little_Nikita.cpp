135875f6bcb1112173418ca78b899e2301d67f52
Ratul1239 <ri373820@gmail.com>
2025-01-06 23:39:09 +0600

    Add files via upload

---
+++ Weak_05/Day_08/A_Little_Nikita.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve()
+{
+    int n,m;
+    cin>>n>>m;
+    if(n%2==0&&m%2==0||n%2==1&&m%2==1)
+    {
+        if(m<=n)cout<<"Yes"<<endl;
+        else cout<<"No"<<endl;
+    }
+    else cout<<"No"<<endl;
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        solve();
+    }
+    return 0;
+}
